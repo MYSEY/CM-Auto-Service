a683c1f1d47e95579eb3739c02e6a244785e54c5
ChhaieangNoun <nounchhaieangdmd@gmail.com>
2025-11-23 23:41:47 -0800

    icon

--- resources/views/layouts/frontend/layouts.blade.php
+++ resources/views/layouts/frontend/layouts.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontends/assets/img/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontends/assets/img/icon.ico') }}">
 
     <!-- CSS
     ========================= -->
