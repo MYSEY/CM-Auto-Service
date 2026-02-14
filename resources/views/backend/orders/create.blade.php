5654bcdde6bfe553193f2c2b968b3b3b437aa73b
MYSEY <mysey1994@gmail.com>
2026-02-14 11:45:57 +0700

    update order

--- resources/views/backend/orders/create.blade.php
+++ resources/views/backend/orders/create.blade.php
@@ -17,7 +17,7 @@
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label for="name">Name</label>
+                                    <label for="name">Customer Name</label>
                                     <span class="text-danger">*</span>
                                     <input type="text" class="form-control" name="name" id="name" placeholder="Enter name" value="{{ old('name') }}">
                                     <p class="text-danger">{!! $errors->first('name') !!}</p>
@@ -25,55 +25,55 @@
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label for="telephone">telephone</label>
-                                    <input type="text" class="form-control" name="telephone" id="telephone" placeholder="Enter telephone" value="{{ old('telephone') }}">       
+                                    <label for="telephone">Telephone</label>
+                                    <input type="text" class="form-control" name="telephone" id="telephone" placeholder="Enter telephone" value="{{ old('telephone') }}">
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label for="content">email</label>
+                                    <label for="content">Email</label>
                                     <input type="text" class="form-control" name="email" id="email" placeholder="Enter email" value="{{ old('email') }}">
                                 </div>
                             </div>
                         </div>
-                        
 
+                        <hr>
+
+                        <h3>Order Details</h3>
                         <div class="row">
                             <div class="col-sm-12">
                                 <table id="tbl_product" class="table table-bordered">
                                     <thead class="">
                                         <tr>
                                             <th>Product</th>
-                                            <th>Product Type</th>
-                                            <th>Category</th>
-                                            <th>Sub Category</th>
-                                            <th>Engine</th>
                                             <th>Quantity</th>
                                             <th>Price</th>
+                                            <th>SubTotal</th>
+                                            <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
                                             <td>
-                                                <select name="product_id" id="product_id" class="form-control">
+                                                <select name="product_code" id="product_code" class="form-control select2">
                                                     <option value="">Please Select</option>
                                                     @foreach($products as $product)
-                                                        <option value="{{ $product->id }}">{{ $product->name }}</option>
+                                                        <option value="{{ $product->code }}">{{ $product->code }}</option>
                                                     @endforeach
                                                 </select>
                                             </td>
-                                            <td><input type="text" class="form-control" name="product_type" placeholder="Enter product type"></td>
-                                            <td><input type="text" class="form-control" name="category" placeholder="Enter category"></td>
-                                            <td><input type="text" class="form-control" name="sub_category" placeholder="Enter sub category"></td>
-                                            <td><input type="text" class="form-control" name="engine" placeholder="Enter engine"></td>
-                                            <td><input type="number" class="form-control" name="quantity" placeholder="Enter quantity"></td>
-                                            <td><input type="number" class="form-control" name="price" placeholder="Enter price"></td>
+                                            <td><input type="number" class="form-control quantity" name="quantity" placeholder="Enter quantity"></td>
+                                            <td><input type="number" class="form-control price" name="price" placeholder="Enter price"></td>
+                                            <td><input type="number" class="form-control sub_total" name="sub_total" placeholder="Enter Sub Total"></td>
+                                            <td>
+                                                <button type="button" class="btn btn-sm btn-success waves-effect waves-themed"><i class="fal fa-plus mr-1"></i> Add New Record</button>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
-                        
+
                         <div class="form-group mb-0" style="text-align: right;">
                             <a href="{{url('admins/order')}}" class="btn btn-outline-secondary btn-pills waves-effect waves-themed">Cancel</a>
                             <button type="submit" class="btn btn-outline-success btn-pills waves-effect waves-themed">Submit</button>
@@ -88,34 +88,17 @@
 @section('script')
     <script>
         $(function(){
-            $(document).on('change','#profile', function() {
-                if (this.files && this.files[0]) {
-                    let img = document.querySelector('.image_preview');
-                    img.onload = () =>{
-                        URL.revokeObjectURL(img.src);
-                    }
-                    img.src = URL.createObjectURL(this.files[0]);
-                    document.querySelector(".image_preview").files = this.files;
-                }
+            $(document).on('change', '#product_code', function () {
+                $('.price').val('');
+                $('.quantity').val('');
+                $('.sub_total').val('');
             });
-            $(document).on('change','#category_id',function(){
-                var category_id = $(this).val();
-                $.ajax({
-                    type: "GET",
-                    url: "{{ url('admins/product/category/onchange') }}",
-                    data: {
-                        category_id:category_id
-                    },
-                    dataType: "JSON",
-                    success: function (response) {
-                        $(".sub_category").empty();
-                        $(".sub_category").empty().append('<option value="">Please Select</option>');
-                        $.each(response.data, function(index, item)
-                        {
-                            $(".sub_category").append('<option value="' + item.id + '">' + item.name + '</option>');
-                        });
-                    }
-                });
+
+            $(document).on('keyup', '.quantity, .price', function () {
+                let quantity = parseFloat($('.quantity').val()) || 0;
+                let price    = parseFloat($('.price').val()) || 0;
+                let totalprice = quantity * price;
+                $('.sub_total').val(totalprice.toFixed(2));
             });
             $(document).on('change','#sub_category_id',function(){
                 var sub_category_id = $(this).val();
--- resources/views/layouts/backend/admin.blade.php
+++ resources/views/layouts/backend/admin.blade.php
@@ -28,6 +28,8 @@
     <link rel="stylesheet" href="{{ asset('backends/css/skins/skin-master.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.7/dist/sweetalert2.min.css" rel="stylesheet">
 
+    <link rel="stylesheet" media="screen, print" href="{{asset('backends/css/formplugins/select2/select2.bundle.css')}}">
+
     {{-- Core Scripts --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
@@ -1058,6 +1060,8 @@
         {{-- toastr --}}
         <script src="{{asset('backends/js/notifications/toastr/toastr.js')}}"></script>
         <script src="{{ asset('backends/js/datagrid/datatables/datatables.bundle.js') }}"></script>
+        <script src="{{asset('backends/js/formplugins/select2/select2.bundle.js')}}"></script>
+
         {!! Toastr::message() !!}
         @yield('script')
 
@@ -1174,6 +1178,123 @@
                     var theadColor = $(this).attr("data-bg");
                     console.log(theadColor);
                     $('#dt-basic-example').removeClassPrefix('bg-').addClass(theadColor);
+                });
+            });
+            $(document).ready(function()
+            {
+                $(function()
+                {
+                    $('.select2').select2();
+
+                    $(".select2-placeholder-multiple").select2(
+                    {
+                        placeholder: "Select State"
+                    });
+                    $(".js-hide-search").select2(
+                    {
+                        minimumResultsForSearch: 1 / 0
+                    });
+                    $(".js-max-length").select2(
+                    {
+                        maximumSelectionLength: 2,
+                        placeholder: "Select maximum 2 items"
+                    });
+                    $(".select2-placeholder").select2(
+                    {
+                        placeholder: "Select a state",
+                        allowClear: true
+                    });
+
+                    $(".js-select2-icons").select2(
+                    {
+                        minimumResultsForSearch: 1 / 0,
+                        templateResult: icon,
+                        templateSelection: icon,
+                        escapeMarkup: function(elm)
+                        {
+                            return elm
+                        }
+                    });
+
+                    function icon(elm)
+                    {
+                        elm.element;
+                        return elm.id ? "<i class='" + $(elm.element).data("icon") + " mr-2'></i>" + elm.text : elm.text
+                    }
+
+                    $(".js-data-example-ajax").select2(
+                    {
+                        ajax:
+                        {
+                            url: "https://api.github.com/search/repositories",
+                            dataType: 'json',
+                            delay: 250,
+                            data: function(params)
+                            {
+                                return {
+                                    q: params.term, // search term
+                                    page: params.page
+                                };
+                            },
+                            processResults: function(data, params)
+                            {
+                                // parse the results into the format expected by Select2
+                                // since we are using custom formatting functions we do not need to
+                                // alter the remote JSON data, except to indicate that infinite
+                                // scrolling can be used
+                                params.page = params.page || 1;
+
+                                return {
+                                    results: data.items,
+                                    pagination:
+                                    {
+                                        more: (params.page * 30) < data.total_count
+                                    }
+                                };
+                            },
+                            cache: true
+                        },
+                        placeholder: 'Search for a repository',
+                        escapeMarkup: function(markup)
+                        {
+                            return markup;
+                        }, // let our custom formatter work
+                        minimumInputLength: 1,
+                        templateResult: formatRepo,
+                        templateSelection: formatRepoSelection
+                    });
+
+                    function formatRepo(repo)
+                    {
+                        if (repo.loading)
+                        {
+                            return repo.text;
+                        }
+
+                        var markup = "<div class='select2-result-repository clearfix d-flex'>" +
+                            "<div class='select2-result-repository__avatar mr-2'><img src='" + repo.owner.avatar_url + "' class='width-2 height-2 mt-1 rounded' /></div>" +
+                            "<div class='select2-result-repository__meta'>" +
+                            "<div class='select2-result-repository__title fs-lg fw-500'>" + repo.full_name + "</div>";
+
+                        if (repo.description)
+                        {
+                            markup += "<div class='select2-result-repository__description fs-xs opacity-80 mb-1'>" + repo.description + "</div>";
+                        }
+
+                        markup += "<div class='select2-result-repository__statistics d-flex fs-sm'>" +
+                            "<div class='select2-result-repository__forks mr-2'><i class='fal fa-lightbulb'></i> " + repo.forks_count + " Forks</div>" +
+                            "<div class='select2-result-repository__stargazers mr-2'><i class='fal fa-star'></i> " + repo.stargazers_count + " Stars</div>" +
+                            "<div class='select2-result-repository__watchers mr-2'><i class='fal fa-eye'></i> " + repo.watchers_count + " Watchers</div>" +
+                            "</div>" +
+                            "</div></div>";
+
+                        return markup;
+                    }
+
+                    function formatRepoSelection(repo)
+                    {
+                        return repo.full_name || repo.text;
+                    }
                 });
             });
         </script>
