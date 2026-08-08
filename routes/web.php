0854b0a1fa598b04c8a8d599c687f1999cd8a375
MYSEY <chhaieangmemot@gmail.com>
2026-08-08 09:35:06 +0700

    fix duplicate category delete photo route

--- routes/web.php
+++ routes/web.php
@@ -149,7 +149,6 @@ Route::group(['prefix' => 'admins', 'middleware' => ['auth']], function () {
 
     Route::delete('/product/{id}/delete-photo', [ProductController::class, 'deletePhoto'])->name('product.delete_photo');
     Route::delete('/category/{id}/delete-photo', [ProductCategoryController::class, 'deletePhoto'])->name('productcate.delete_photo');
-    Route::delete('/category/{category}/delete-photo', [ProductCategoryController::class, 'deletePhoto'])->name('productcate.delete_photo');
     Route::delete('/gallery-image/{id}/delete', [ProductController::class, 'deleteGalleryImage'])->name('product.delete_gallery_image');
     Route::post('admins/slide/change/status/{id}', [SliderController::class, 'changeStatus'])->name('admins.slide.change.status');
 
